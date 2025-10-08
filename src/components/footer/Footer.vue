b3717c1f9c87b3ab1c896b9af9a97e645247e738
Daniel Jonker <ddavidjonker@outlook.com>
2025-10-08 23:03:43 +0200

    Better footer and translations

--- src/components/footer/Footer.vue
+++ src/components/footer/Footer.vue
@@ -1,33 +1,38 @@
 <template>
     <div class="w-100 bg-primary grid grid-cols-12 gap-20 text-primary-contrast py-20">
         <div class="col-span-3 col-start-2">
-            <Logo class="pb-4" />
+            <Logo class="pb-8" />
             <div class="pb-8">{{ $gettext('footer.text') }}</div>
             <div class="align-bottom">{{ currentYear }} | Inter-Actief</div>
         </div>
 
         <div class="col-span-2">
-            <div class="font-bold pb-2">Pages</div>
-            <div v-for="item in pages" :key="item.name" class="py-1">
+            <div class="font-bold pb-2">{{ $gettext('Pages') }}</div>
+            <div v-for="item in pages" :key="item.name">
                 <RouterLink :to="{ name: item.page }" class="link">{{ item.name }}</RouterLink>
             </div>
         </div>
 
         <div class="col-span-2">
-            Nog wat tekst
-
-            Nog wat tekst
+            <div class="font-bold pb-2">{{ $gettext('Important information') }}</div>
+            <div><a class="link" href="https://privacy.ia.utwente.nl/">{{ $gettext('Privacy policy') }}</a></div>
+            <div>
+                <RouterLink :to="{ name: 'contact' }" class="link">{{ $gettext('Contact') }}</RouterLink>
+            </div>
+            <div><a class="link" href="https://github.com/Inter-Actief/amelink/issues">
+                    {{ $gettext('Report a problem') }}</a></div>
+            <div>
+                <RouterLink to="/about/33/coordinated-vulnerability-disclosure-policy/" class="link">
+                    {{ $gettext('CVD Policy') }}</RouterLink>
+            </div>
         </div>
 
         <div class="col-span-2">
-            <h4>{{ $gettext('Contact us') }}</h4>
-
-            <ul>
-                <li>Zilverling, kamer E208</li>
-                <li>Drienerlolaan 5</li>
-                <li>7522 NB Enschede</li>
-                <li>The Netherlands</li>
-            </ul>
+            <div class="font-bold pb-2">{{ $gettext('Contact us') }}</div>
+            <div>Zilverling, kamer E208</div>
+            <div>Drienerlolaan 5</div>
+            <div>7522 NB Enschede</div>
+            <div>The Netherlands</div>
         </div>
     </div>
 </template>
