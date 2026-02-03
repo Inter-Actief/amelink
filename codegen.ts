c9fb969af0f8460d32f5bf3987946211d2087f2b
Daniel Jonker <ddavidjonker@outlook.com>
2026-02-03 21:24:32 +0100

    Remove dotenv and replace with dotenv-flow, also add a small message from codegen.ts

--- codegen.ts
+++ codegen.ts
@@ -1,6 +1,9 @@
 import type { CodegenConfig } from '@graphql-codegen/cli'
-import dotenv from 'dotenv'
-dotenv.config()
+import dotenvFlow from 'dotenv-flow';
+dotenvFlow.config();
+
+// Can be overwritten in a .env.local file!
+console.log(`Fetching graphQL schema from ${process.env.VITE_AMELIE_GRAPHQL_API}`)
 
 const config: CodegenConfig = {
     overwrite: true,
