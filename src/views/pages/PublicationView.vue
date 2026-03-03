08b8f2cd7708835d56ca8e72fe0f7cc526e0b597
Bart Oostveen <bart.oos.2006@gmail.com>
2026-03-03 21:39:45 +0100

    feat: add type dropdown to publications (#50)
    This fixes https://github.com/Inter-Actief/amelink/issues/32
    TODO: merge https://github.com/Inter-Actief/amelie/pulls/1112 first!

--- src/views/pages/PublicationView.vue
+++ src/views/pages/PublicationView.vue
@@ -1,6 +1,51 @@
+<script setup lang="ts">
+import Content from '@/components/ui/Content.vue';
+import SectionCard from '@/components/ui/SectionCard.vue';
+import { computed, ref, effect } from 'vue';
+import { useGettext } from 'vue3-gettext';
+const { $gettext } = useGettext();
+import { formattedData } from '@/functions/functions.ts'
+import Pagination from '@/components/ui/Pagination.vue';
+import { useQuery } from '@/composables/queries';
+
+const publicationUrl = (url: string) => `${import.meta.env.VITE_AMELIE_MEDIA_URL}${url}`;
+const limit = ref(100);
+const type = ref<string | undefined>(undefined);
+const query = useQuery('publicationOverview', { limit: limit.value, type: type.value });
+const totalCount = computed(() => query.result.value?.publications?.totalCount);
+const queryItems = computed(() => query.result.value?.publications?.results);
+const { loading, refetch } = query;
+const types = computed(() => query.result.value?.publicationTypes?.map(p => p?.typeName) ?? []);
+effect(() => {
+    refetch({ limit: limit.value, type: type.value });
+});
+</script>
+
+<style scoped>
+.text-card {
+    &:hover .expand-link {
+        background-size: 100% 0.2rem;
+    }
+}
+</style>
+
 <template>
     <Content>
         <SectionCard :name="$gettext('Publications')">
+            <template #info>
+                <Select v-model="type" :options="types" showClear optionLabel="name" placeholder="" class="w-full">
+                    <template #value="slotProps">
+                        <div class="flex items-center p-1">
+                            {{ type ? type : "Filter..." }}
+                        </div>
+                    </template>
+                    <template #option="slotProps">
+                        <div class="flex items-center">
+                            {{ slotProps.option }}
+                        </div>
+                    </template>
+                </Select>
+            </template>
             <template #content>
                 <ProgressSpinner v-if="loading" />
                 <div v-else class="grid grid-cols-5 gap-10 pb-4">
@@ -30,31 +75,3 @@
         </SectionCard>
     </Content>
 </template>
-
-<script setup lang="ts">
-import Content from '@/components/ui/Content.vue';
-import SectionCard from '@/components/ui/SectionCard.vue';
-import { computed, ref } from 'vue';
-import { useGettext } from 'vue3-gettext';
-const { $gettext } = useGettext();
-import { formattedData } from '@/functions/functions.ts'
-import Pagination from '@/components/ui/Pagination.vue';
-import { useQuery } from '@/composables/queries';
-
-const publicationUrl = (url: string) => `${import.meta.env.VITE_AMELIE_MEDIA_URL}${url}`;
-const limit = ref(10)
-const query = useQuery('publicationOverview', { limit: limit.value });
-const totalCount = computed(() => query.result.value?.publications?.totalCount);
-const queryItems = computed(() => {
-    return query.result.value?.publications?.results;
-})
-const { loading } = query;
-</script>
-
-<style scoped>
-.text-card {
-    &:hover .expand-link {
-        background-size: 100% 0.2rem;
-    }
-}
-</style>
