018310be99e9fcee3df9b60b42e6adb39ff2dce9
Bart Oostveen <bart@bartoostveen.nl>
2026-03-30 21:54:57 +0200

    chore: add 404 page

--- src/router/index.ts
+++ src/router/index.ts
@@ -11,6 +11,11 @@ const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
         {
+            path: "/:path(.*)*",
+            name: "notfound",
+            component: () => import('@/views/404.vue')
+        },
+        {
             path: '/',
             name: 'home',
             component: () => HomePage,
