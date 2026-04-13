22bfc06bd211b8139bad8b229cda0a97ca0208f5
Daniel Jonker <ddavidjonker@outlook.com>
2026-04-13 21:11:05 +0200

    Add manual chunks

--- vite.config.ts
+++ vite.config.ts
@@ -28,4 +28,23 @@ export default defineConfig({
         },
     },
     css: {},
+    build: {
+        // Target should be es2020 or above (if want to use import.meta)
+        // https://esbuild.github.io/content-types/#javascript
+        target: 'esnext', // Newest JS
+        rollupOptions: {
+            output: {
+                // Setup manual chunks such that the content is not server in one
+                manualChunks(id) {
+                    if (id.includes('@vue')) return 'vue';
+                    if (id.includes('@apollo/client')) return 'apollo';
+                    if (id.includes('@primeuix')) return 'primeui';
+                    if (id.includes('@primevue')) return 'primevue';
+                    if (id.includes('@graphql')) return 'graphql';
+                    if (id.includes('@pinia')) return 'pinia';
+                    if (id.includes('@lucide')) return 'lucide';
+                }
+            }
+        }
+    }
 })
