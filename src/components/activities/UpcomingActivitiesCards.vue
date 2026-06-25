82e56756ca71b0dfead4067b7dedb982399602ba
Daniel Jonker <ddavidjonker@outlook.com>
2026-06-25 20:34:46 +0200

    Added staggared grid and loading (#91)
    * Added staggared grid and loading
    
    * Small fix

--- src/components/activities/UpcomingActivitiesCards.vue
+++ src/components/activities/UpcomingActivitiesCards.vue
@@ -43,7 +43,7 @@ import { useGettext } from 'vue3-gettext'
 import SectionCard from '../ui/SectionCard.vue'
 import Pagination from '../ui/Pagination.vue'
 import TextCard from '../ui/TextCard.vue'
-import { formattedData, getItemValue } from '@/functions/functions.ts'
+import { formattedData } from '@/functions/functions.ts'
 import { useQuery, useQueryAsync } from '@/composables/queries'
 import { imageSrc } from '@/composables/util'
 const { $gettext } = useGettext();
