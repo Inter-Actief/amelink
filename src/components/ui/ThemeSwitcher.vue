11d0baccbbf616ee93630df47d90ef00860cf68b
Daniel Jonker <ddavidjonker@outlook.com>
2026-08-19 17:41:45 +0200

    Added language stuff

--- src/components/ui/ThemeSwitcher.vue
+++ src/components/ui/ThemeSwitcher.vue
@@ -8,10 +8,13 @@
 import { Sun, Moon, MonitorCog } from '@lucide/vue'
 import { computed } from 'vue'
 import { useThemeStore } from '@/stores/themeStore'
+import { useGettext } from 'vue3-gettext'
 
 const themeStore = useThemeStore()
 const theme = computed(() => themeStore.theme)
 const { switchTheme } = themeStore
+const gettext = useGettext();
+const { $gettext } = gettext;
 
 const icon = computed(() => {
     switch (theme.value) {
@@ -27,11 +30,11 @@ const icon = computed(() => {
 const title = computed(() => {
     switch (theme.value) {
         case 'light':
-            return 'Light mode'
+            return $gettext('Light mode')
         case 'dark':
-            return 'Dark mode'
+            return $gettext('Dark mode')
         default:
-            return 'Use system theme'
+            return $gettext('Use system theme')
     }
 })
 </script>
